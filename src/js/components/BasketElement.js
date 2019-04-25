ac0728ad00f8634fa7ff047637b6d0472f5fb41e
Dmytro Bagazhkov <36930434+d-bagazhkov@users.noreply.github.com>
2019-04-25 13:06:05 +0300

    fix Shopping Cart

--- src/js/components/BasketElement.js
+++ src/js/components/BasketElement.js
@@ -7,6 +7,18 @@ export default class BasketElement extends Component {
 
     phonesInfo = {};
 
+    style = {
+        item: `
+            list-style-type: none
+            `,
+        press: `
+            border: 1px black solid; 
+            width: 2rem; 
+            border-radius: 50%;
+            text-align: center;
+            cursor: pointer`
+    }
+
     constructor(props) {
         super(props);
 
@@ -17,14 +29,22 @@ export default class BasketElement extends Component {
             this.drawing();
         });
 
-        this.on("click", "[data-phone-id]", ({ target }) => {
-            this.props.values = this.props.values.filter(id => id != target.dataset.phoneId);
+        this.on("click", "[data-action-delete]", ({ target }) => {
+            this.props.values = this.props.values.filter(id => id != target.closest("[data-phone-id]").dataset.phoneId);
             this.drawing();
         })
-    }
 
-    beforeRender() {
-        this.props.values = this.uniqueArray(this.props.values)
+        this.on("click", "[data-action-add]", ({ target }) => {
+            this.props.values.push(target.closest("[data-phone-id]").dataset.phoneId);
+            this.drawing();
+        })
+
+        this.on("click", "[data-action-remove]", ({ target }) => {
+            let id = target.closest("[data-phone-id]").dataset.phoneId;
+            let indexId = this.props.values.indexOf(id);
+            this.props.values.splice(indexId, 1);
+            this.drawing();
+        })
     }
 
     uniqueArray(list) {
@@ -42,8 +62,18 @@ export default class BasketElement extends Component {
         <p>Shopping Cart</p>
         <ul>
             ${this.props.values && this.props.values.length
-                ? this.props.values.map(phoneId =>
-                    `<li class=".addedItem" data-phone-id="${phoneId}">${this.phonesInfo[phoneId]}</li>`
+                ? this.uniqueArray(this.props.values).map(phoneId =>
+                    `<li style="${this.style.item}"  data-phone-id="${phoneId}">
+                        <label>${this.props.values.reduce((store, id) => {
+                        if (id === phoneId)
+                            return store + 1;
+                        return store;
+                    }, 0)}</label>
+                        <label style="${this.style.press}" data-action-add>+</label>
+                        <label style="${this.style.press}" data-action-remove>-</label>
+                        <label style="${this.style.press}" data-action-delete>X</label>
+                        ${this.phonesInfo[phoneId]}
+                    </li>`
                 ).join('')
                 : '<li>Empty</li>'}
         </ul>
